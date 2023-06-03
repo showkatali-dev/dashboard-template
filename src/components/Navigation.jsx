70a72975eddf241c125bca7d4f3384242383222b
coder-showkat <showkat.coder@gmail.com>
2023-06-03 11:32:58 +0600

    added logo to navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import { BsFillPersonFill, BsPieChartFill } from "react-icons/bs";
 import { FaBlog, FaBriefcase } from "react-icons/fa";
 import { RiMailOpenFill } from "react-icons/ri";
 import { NavLink } from "react-router-dom";
+import logo from "../assets/Logo-1-1.png";
 
 const Menu = () => {
   return (
@@ -27,7 +28,8 @@ const Menu = () => {
 
 const Navigation = () => {
   return (
-    <div className="fixed left-0 top-0 bottom-0 bg-base-100 h-screen flex flex-col items-center p-2 justify-start gap-6 z-50 nav">
+    <div className="fixed left-0 top-0 bottom-0 bg-base-100 h-screen flex flex-col items-center p-2 pl-0 justify-start gap-6 z-50 nav">
+      <img src={logo} alt="logo" className="w-10 pl-0.5" />
       <Menu />
     </div>
   );
